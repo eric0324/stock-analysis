1a86286c4281e535a90aaec8e17846fc3d4526b4
Eric Wu <smart032410@gmail.com>
2023-11-25 06:51:05 +0800

    feat: 自動化收集股市行情

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,46 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger, OnApplicationBootstrap } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import {MongooseModule} from "@nestjs/mongoose";
-import * as process from "process";
-import {ScraperModule} from "./scraper/scraper.module";
+import { ScheduleModule } from '@nestjs/schedule';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DateTime } from 'luxon';
+import { ScraperModule } from './scraper/scraper.module';
 import { MarketStatsModule } from './market-stats/market-stats.module';
 import { TickerModule } from './ticker/ticker.module';
-import {ScheduleModule} from "@nestjs/schedule";
+import { MarketStatsService } from './market-stats/market-stats.service';
+import { TickerService } from './ticker/ticker.service';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [
-      ConfigModule.forRoot(),
-      MongooseModule.forRoot(process.env.MONGODB_URI),
-      ScraperModule,
-      MarketStatsModule,
-      TickerModule,
-      ScheduleModule.forRoot(),
-  ]
+    imports: [
+        ConfigModule.forRoot(),
+        ScheduleModule.forRoot(),
+        MongooseModule.forRoot(process.env.MONGODB_URI),
+        ScraperModule,
+        MarketStatsModule,
+        TickerModule,
+        ReportModule,
+    ]
 })
-export class AppModule {}
+export class AppModule implements OnApplicationBootstrap {
+    constructor(
+        private readonly marketStatsService: MarketStatsService,
+        private readonly tickerService: TickerService
+    ) {}
+
+    async onApplicationBootstrap() {
+        if (process.env.SCRAPER_INIT_ENABLED === 'true') {
+            Logger.log('正在初始化應用程式...', AppModule.name);
+
+            const days = parseInt(process.env.SCRAPER_INIT_DAYS, 10) || 30;
+            const startDate = DateTime.local().minus({ days });
+            const endDate = DateTime.local();
+
+            for (let dt = startDate; dt <= endDate; dt = dt.plus({ day: 1 })) {
+                await this.marketStatsService.updateMarketStats(dt.toISODate());
+                await this.tickerService.updateTickers(dt.toISODate());
+            }
+
+            Logger.log('應用程式初始化完成', AppModule.name);
+        }
+    }
+}
